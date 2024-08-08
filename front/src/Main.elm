1baf7badb80c8bbebbd67b48256a8545098fc375
Roman Matveev <roman.matveev@mysky.com>
2024-08-08 20:30:21 +0400

    fucking css

--- front/src/Main.elm
+++ front/src/Main.elm
@@ -7,6 +7,10 @@ import Browser
 import Html exposing (button)
 import Html.Events exposing (onClick)
 import Html exposing (text)
+import List exposing (length)
+import Html exposing (span)
+import List exposing (foldl)
+import List exposing (map)
 
 type  Dot = Empty|Red|Blue
 
@@ -20,6 +24,27 @@ type alias Model =
 type Msg = 
     PlaceDot 
 
+
+get_color dot =
+    case dot of 
+        Empty -> "white"
+        Blue -> "blue"
+        Red -> "red"
+
+makeCircle : String -> Dot -> String -> Html Msg
+makeCircle size dot margin =
+    span
+        [ style "width" size
+        , style "height" size
+        , style "background-color" (get_color dot)
+        , style "border-radius" "50%"
+        , style "border-block-style" "solid"
+        , style "margin" margin
+        , style "display" "inline-table"
+        ]
+        [span [onClick PlaceDot] [ text "."]]
+
+-- INIT
 init : Model
 init = 
     {
@@ -27,29 +52,26 @@ init =
         width = 10,
         lengnth = 10
     }
-makeCircle : String -> String -> String -> Html Msg
-makeCircle size color margin =
-    div
-        [ style "width" size
-        , style "height" size
-        , style "background-color" color
-        , style "border-radius" "50%"
-        , style "margin" margin
-        ]
-        [button [onClick PlaceDot] [ text "PlaceDot"]]
 
-
+-- UPDATE
 update: Msg -> Model -> Model
 update msg model =
     case msg of
         PlaceDot -> model
+
+-- VIEW
+
+spanify: List Dot -> Html Msg
+spanify row =
+    div [] 
+        (List.map (\r -> makeCircle "50px" Red "10px") row)
+    
+get_dots : List (List Dot) -> List (Html Msg)
+get_dots grid =
+    List.map spanify grid
 view : Model -> Html Msg
 view state =
-    div []
-        [ makeCircle "100px" "red" "10px"
-        , makeCircle "100px" "blue" "10px"
-        , makeCircle "100px" "green" "10px"
-        ]
+    div [] (get_dots state.grid)
 
 main : Program () Model Msg
 main = 
